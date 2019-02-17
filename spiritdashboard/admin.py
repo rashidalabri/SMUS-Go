ef42375d98356ffb1a59437580a469f583f69351
Rashid Al-Abri <itsrashidalabri@gmail.com>
2019-02-17 02:01:40 -0800

    Remove avatar field from User model admin page

--- spiritdashboard/admin.py
+++ spiritdashboard/admin.py
@@ -5,7 +5,7 @@ from .models import CompletedMission, Mission, MissionKey, User, Grade
 
 class SpiritUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-            ('Spirit Data', {'fields': ('grade','points','total_xp','avatar')}),
+            ('Spirit Data', {'fields': ('grade','points','total_xp')}),
     )
 
 
